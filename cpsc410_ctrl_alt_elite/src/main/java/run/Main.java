1cb6aa463a1725c0325cd1f0ffe6415aa934b8fd
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2019-10-02 16:16:25 -0700

    Added all of Ceylin's changes that weren't showing up and modified Main to include the parsing.

--- cpsc410_ctrl_alt_elite/src/main/java/run/Main.java
+++ cpsc410_ctrl_alt_elite/src/main/java/run/Main.java
@@ -10,7 +10,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> literals = Arrays.asList("from");
+        Tokenizer.makeTokenizer("input.tdot",literals);
+        Node program = new PROGRAM();
+        program.parse();
     }
 
 }
