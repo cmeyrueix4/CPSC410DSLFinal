7dabc5a963849260568adb4023f4d9b5f85b60d1
Ben Henaghan (psybth) <psybth@exmail.nottingham.ac.uk>
2019-10-03 11:02:39 -0700

    un-commented opencv code

--- cpsc410_ctrl_alt_elite/src/mainrun/Main.java
+++ cpsc410_ctrl_alt_elite/src/mainrun/Main.java
@@ -3,18 +3,17 @@ package mainrun;
 import ast.PROGRAM;
 import libs.Node;
 import libs.Tokenizer;
+import org.opencv.core.Core;
 
 import java.util.Arrays;
 import java.util.List;
 
-//import org.opencv.core.Core;
-
 public class Main {
 
-//    static {
-//        // Loads the OpenCV Library
-//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//    }
+    static {
+        // Loads the OpenCV Library
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
 
     public static void main(String[] args) {
         List<String> literals = Arrays.asList(",", "load", "save", "create", "filter", "all", "collage", "from", ":", "gif", "end");
