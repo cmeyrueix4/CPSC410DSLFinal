cf5b8d77a7209940aafc28c579a448fa0e5950ca
Ben Henaghan (psybth) <psybth@exmail.nottingham.ac.uk>
2019-10-12 15:29:12 -0700

    Revert "Added some more filters"
    This reverts commit 35ffe66622a32fc95a8774ec960a8c429f0c8e89.

--- cpsc410_ctrl_alt_elite/src/filter/Vignette.java
+++ cpsc410_ctrl_alt_elite/src/filter/Vignette.java
@@ -2,13 +2,10 @@ package filter;
 
 import org.opencv.core.Mat;
 
-public class Contrast implements Filter {
-
+public class Vignette implements Filter {
     @Override
     public Mat process(Mat inputImage) {
-        Mat output = inputImage.clone();
-        inputImage.convertTo(output, -1, 4, -100);
-        return output;
+        return inputImage;
     }
 
     @Override
