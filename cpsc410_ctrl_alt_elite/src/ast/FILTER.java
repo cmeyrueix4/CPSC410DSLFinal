8320dd1a1c77b27fac32aa606b11918e5ac99bd8
Ben Henaghan (psybth) <psybth@exmail.nottingham.ac.uk>
2019-10-12 13:45:22 -0700

    Link up the filter code to the parsing code WIP

--- cpsc410_ctrl_alt_elite/src/ast/FILTER.java
+++ cpsc410_ctrl_alt_elite/src/ast/FILTER.java
@@ -1,25 +1,47 @@
 package ast;
 
+import filter.Blur;
+import filter.Filter;
+import filter.Sharpen;
+import filter.Vignette;
+import org.opencv.core.Mat;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class FILTER extends STATEMENT{
+public class FILTER extends STATEMENT {
+
+    private enum ImageFilters {
+        BLUR("blur", new Blur()),
+        SHARPEN("sharpen", new Sharpen()),
+        VIGNETTE("vignette", new Vignette());
+
+        private final String tokenName;
+        private final Filter filter;
+
+        ImageFilters(String tokenName, Filter filter) {
+            this.tokenName = tokenName;
+            this.filter = filter;
+        }
+
+    }
+
     String filterOption;
-    String photo;
     List<String> photos = new ArrayList<>();
 
     @Override
     public void parse() {
+        String photo;
+
         tokenizer.getAndCheckNext("filter");
         filterOption = tokenizer.getNext();
 
         tokenizer.getAndCheckNext(":");
-        if (tokenizer.checkToken("all")){
+        if (tokenizer.checkToken("all")) {
             photo = tokenizer.getNext();
             photos.add(photo);
-        }
-        else {
-            while(!tokenizer.checkToken("end")){
+        } else {
+            while (!tokenizer.checkToken("end")) {
                 photo = tokenizer.getNext();
                 photos.add(photo);
             }
@@ -27,11 +49,14 @@ public class FILTER extends STATEMENT{
         }
 
 
-
     }
 
     @Override
     public void evaluate() {
-
+        for (ImageFilters f : ImageFilters.values()) {
+            if (filterOption.equals(f.tokenName)) {
+                Mat ret = f.filter.process(null); // TODO
+            }
+        }
     }
 }
