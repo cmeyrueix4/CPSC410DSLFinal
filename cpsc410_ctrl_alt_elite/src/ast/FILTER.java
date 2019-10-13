33b0da5e6ecd1071eecb862c88a5059c28d7a640
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2019-10-12 19:00:59 -0700

    Idk what has happened x2

--- cpsc410_ctrl_alt_elite/src/ast/FILTER.java
+++ cpsc410_ctrl_alt_elite/src/ast/FILTER.java
@@ -4,11 +4,19 @@ import filter.Blur;
 import filter.Filter;
 import filter.Sharpen;
 import filter.Vignette;
-import org.opencv.core.Mat;
 
 import libs.NameCheckException;
 import mainrun.Main;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,8 +51,7 @@ public class FILTER extends STATEMENT {
         if (tokenizer.checkToken("all")) {
             photo = tokenizer.getNext();
             photos.add(photo);
-        }
-        else {
+        } else {
             String first = tokenizer.getNext();
             photos.add(first);
             while (tokenizer.checkToken(",")) {
@@ -61,17 +68,34 @@ public class FILTER extends STATEMENT {
     public void evaluate() {
         for (ImageFilters f : ImageFilters.values()) {
             if (filterOption.equals(f.tokenName)) {
-                Mat ret = f.filter.process(null);
+                Mat ret = f.filter.process(null); // TODO
+
+                // Convert from bufferedimage to mat
+                BufferedImage bi = null; // TODO
+                Mat img = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+                byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+                img.put(0, 0, data);
+
+                // Convert back to bufferedImage
+                BufferedImage output = null;
+                MatOfByte mob = new MatOfByte();
+                Imgcodecs.imencode(".jpg", ret, mob);
+                try {
+                    output = ImageIO.read(new ByteArrayInputStream(mob.toArray()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     @Override
     public void nameCheck() {
-        for (String s: photos) {
-            if(!Main.variables.containsKey(s)){
+        for (String s : photos) {
+            if (!Main.variables.containsKey(s)) {
                 throw new NameCheckException(s);
             }
         }
+
     }
 }
--- cpsc410_ctrl_alt_elite/src/mainrun/Main.java
+++ cpsc410_ctrl_alt_elite/src/mainrun/Main.java
@@ -19,13 +19,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-//        Main.class.getResource("input.txt");
         List<String> literals = Arrays.asList("load", ",", "save", "create", "filter", "all", "collage", "from", ":", "gif");
         Tokenizer.makeTokenizer("input.txt",literals);
-//        Tokenizer.makeTokenizer(String.valueOf(Main.class.getResource("../../input.txt")), literals);
-//        Tokenizer.makeTokenizer("/Users/ceylin/Documents/UBC/CPSC/410/CPSC410DSL/cpsc410_ctrl_alt_elite/input.txt",literals);
-//        Tokenizer.makeTokenizer("C:\\Users\\Cyrielle\\IdeaProjects\\CPSC410DSLV2\\cpsc410_ctrl_alt_elite\\input.txt",literals);
-//        Node.setWriter("C:\\Users\\HP USER\\Desktop\\CPSC410DSL\\cpsc410_ctrl_alt_elite\\gifOutput");
         Node program = new PROGRAM();
         program.parse();
         program.nameCheck();
